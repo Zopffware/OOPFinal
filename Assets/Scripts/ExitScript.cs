e8830540d8738335e9faa334f646bf45ed6e3201
Kaedian <KMaxfield@student.neumont.edu>
2018-02-27 12:39:20 -0700

    Exit button update
    Created a script for the exit button and applied it so we can actually close our program easily.

---
+++ Assets/Scripts/ExitScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ExitScript : MonoBehaviour {
+    public Button Exit;
+    // Use this for initialization
+    void Start () {
+        Button btn = Exit.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+
+
+}
