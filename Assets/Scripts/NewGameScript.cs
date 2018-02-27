9674b1e08833bb359bcc7058a6c959a66f9878bc
Kaedian <KMaxfield@student.neumont.edu>
2018-02-27 16:55:08 -0700

    Scene Transition
    Made it so that when New game is pressed, it takes you to the information Scene.

---
+++ Assets/Scripts/NewGameScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewGameScript : MonoBehaviour {
+    public Button NewGame;
+	// Use this for initialization
+	void Start () {
+        Button btn = NewGame.GetComponent<Button>();
+        btn.onClick.AddListener(GameStart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GameStart () {
+        SceneManager.LoadScene("IntroScene", LoadSceneMode.Single);
+        
+    }
+}
