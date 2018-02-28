74f38f4e8b3846a3a5550d026434f2c1e4386a80
Kaedian <KMaxfield@student.neumont.edu>
2018-02-28 14:27:22 -0700

    Transfering second scene to new prefab

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ControllerScript : MonoBehaviour {
+public class ControllerScript : MonoBehaviour
+{
+    public Transform PlayerGender;
+    public Transform PlayerPreferance;
+    InputField NameField;
+    public string sex;
+    public string preferedSex;
+    public string playerName;
+    // Use this for initialization
+    void Start()
+    {
+        int menuIndex = PlayerGender.GetComponent<Dropdown>().value;
+        List<Dropdown.OptionData> menuOptions = PlayerGender.GetComponent<Dropdown>().options;
+        sex = menuOptions[menuIndex].text;
+        menuOptions = PlayerPreferance.GetComponent<Dropdown>().options;
+        preferedSex = menuOptions[menuIndex].text;
+        playerName = NameField.text;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
