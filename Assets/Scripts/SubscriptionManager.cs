1392fb0230001eb0a6c977bffaecac02c26066cb
Michael Zopff <michael.zopff@gmail.com>
2018-03-01 12:35:34 -0700

    Add Subscription Manager

---
+++ Assets/Scripts/SubscriptionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubscriptionManager {
+	private Dictionary<IPublisher, List<ISubscriber>> subscriptions = new Dictionary<IPublisher, List<ISubscriber>>();
+
+	public void notifySubscribers(IPublisher publisher) {
+		List<ISubscriber> subscribers = subscriptions[publisher];
+		foreach (ISubscriber subscriber in subscribers) {
+			subscriber.notify();
+		}
+	}
+	public void registerPublisher(IPublisher publisher) {
+		subscriptions[publisher] = new List<ISubscriber>();
+	}
+	public void subscribe(ISubscriber subscriber, IPublisher publisher) {
+		subscriptions[publisher].Add(subscriber);
+	}
+}
