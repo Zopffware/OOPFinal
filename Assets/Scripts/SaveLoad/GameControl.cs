ae776add907046a663ded40549eeac39680238e1
Kaedian <KMaxfield@student.neumont.edu>
2018-03-07 16:22:40 -0700

    Revert "minor adjust to save/load"
    This reverts commit e98f73fc086f7351d65556324b0f44fb2f9ea832.

--- Assets/Scripts/SaveLoad/GameControl.cs
+++ Assets/Scripts/SaveLoad/GameControl.cs
@@ -15,7 +15,6 @@ public class GameControl : MonoBehaviour {
     public int CSLovePoints;
     public int CPPLovePoints;
     public int PYLovePoints;
-    public int money;
     public int day;
     public int hour;
 
