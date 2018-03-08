4601b2b7f3e0414efafcd29a4d225fb62c3969e3
Jacob Castro <36865238+JCastro99@users.noreply.github.com>
2018-03-08 16:22:12 -0700

    store now displays current money on opening

---
+++ Assets/currentMoney.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class currentMoney : MonoBehaviour {
+    public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		 int cm = GameControl.control.money;
+        
+        countText.text="ByteCoin: "+cm.ToString ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int cm = GameControl.control.money;
+        string myString = cm.ToString();
+        GameObject.Find("currentMoney").GetComponentInChildren<Text>().text=myString;
+	}
+}
