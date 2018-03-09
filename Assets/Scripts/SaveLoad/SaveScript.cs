32bdb440e37bb8e7389e61ea060dbb53221b0295
Kaedian <KMaxfield@student.neumont.edu>
2018-03-09 16:02:08 -0700

    Escape Key Menu
    With Save button!

---
+++ Assets/Scripts/SaveLoad/SaveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SaveScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(save);
+    }
+
+    void save()
+    {
+        GameControl.control.Save();
+    }
+
+}
