aa3aff55e4086ceeb9d9bb9776154c42aae0b0e2
Michael Zopff <michael.zopff@gmail.com>
2018-03-12 11:27:42 -0600

    Fixed Warnings :department_store:

--- Assets/Scripts/SaveLoad/GameControl.cs
+++ Assets/Scripts/SaveLoad/GameControl.cs
@@ -31,7 +31,6 @@ public class GameControl : MonoBehaviour
         Inventory = FindObjectOfType<Invetory>();
         if (control == null)
         {
-            DontDestroyOnLoad(gameObject);
             control = this;
         }
         else if (control != this)
